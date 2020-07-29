428e947e7732f55fc9db1248fc65feeea94d3c47
Israel Lazo <israel.lazo@peanuthuib.cl>
2020-07-28 21:56:06 -0400

    package update, hello function

---
+++ functions/hello.js
@@ -0,0 +1,5 @@
+/* export our lambda function as named "handler" export */
+exports.handler = async (event, context) => {
+  /* configure faunaDB Client with our secret */
+  console.log("HOOOLAAA POOOOO!!!")
+}
